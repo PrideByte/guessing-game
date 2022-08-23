25f61eb51202632287ba89028d948b24c77151fb
PrideByte <alpha5.2@mail.ru>
2022-08-23 15:19:02 +0300

    feat: task solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let range = this.max - this.min;
+        return this.assumption = Math.ceil(range / 2) + this.min;
     }
 
     lower() {
-
+        this.max = this.assumption;
     }
 
     greater() {
-
+        this.min = this.assumption;
     }
-}
+} 
 
 module.exports = GuessingGame;
